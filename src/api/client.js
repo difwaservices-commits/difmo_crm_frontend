dea105727b67a75a13c1c3405aad4f30a095e297
ramjeaimt <ramjeeaimt@gmail.com>
2026-04-15 01:14:12 +0530

    update ui1

--- src/api/client.js
+++ src/api/client.js
@@ -8,7 +8,7 @@ const apiClient = axios.create({
     },
 });
 
-// Request interceptor
+// Request 
 apiClient.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token');
